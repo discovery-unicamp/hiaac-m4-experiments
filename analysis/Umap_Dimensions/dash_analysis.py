906d58402813c98288f2a0e04f8080921737348e
Patrick Alves <tricsantos88@gmail.com>
2023-01-18 13:03:26 -0300

    Update the dash to analyze umap results

--- analysis/Umap_Dimensions/dash_analysis.py
+++ analysis/Umap_Dimensions/dash_analysis.py
@@ -178,4 +178,4 @@ def update_chart(metric, domain, classifier, dataset):
     return fig
  
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050)
+    app.run_server(port=8050, debug=True, use_reloader=True)
