637958107588b75331e1e6b967d3e17f12c6d86d
Otavio Napoli <otavio.napoli@gmail.com>
2023-01-18 17:00:54 +0000

    Added execution_2 experiments

--- experiments/Umap_Dimensions/run_experiment.sh
+++ experiments/Umap_Dimensions/run_experiment.sh
@@ -1,1 +1,1 @@
-python execute.py executions_fft_only.yaml -d "/home/otavio/hiaac/balanced_20Hz_filtered" -o "./fft_results" --address='194.210.86.16:6379' --start 0 --end 10000
+python execute.py executions_2.yaml -d "/home/otavio/hiaac/balanced_20Hz_filtered" -o "./config2_results" 
